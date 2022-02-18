c7fd8a6743bfd927571a0de8ef83fbde31e65a71
banishlight <banishlight@gmail.com>
2022-02-17 23:47:07 -0600

    First commit.  Basic bot makes SCV's

---
+++ src/main/java/BanishBot.java
@@ -0,0 +1,53 @@
+import bwapi.*;
+
+public class BanishBot extends DefaultBWListener{
+    private BWClient bwClient;
+    private Game game;
+    private Player self;
+
+    @Override
+    public void onStart(){
+        game = bwClient.getGame();
+        self = game.self();
+    }
+
+    @Override
+    public void onFrame(){
+        game.drawTextScreen(100, 100, "BanishBot v1.0!");
+        // Train units while we can
+        for (Unit trainer : self.getUnits()) {
+            UnitType unitType = trainer.getType();
+            if (unitType.isBuilding() && !unitType.buildsWhat().isEmpty()) {
+                UnitType toTrain = unitType.buildsWhat().get(0);
+                if (game.canMake(toTrain, trainer)) {
+                    trainer.train(toTrain);
+                }
+            }
+        }
+    }
+
+    void run() {
+        bwClient = new BWClient(this);
+        bwClient.startGame();
+    }
+
+    public static void main(String[] args) {
+        new BanishBot().run();
+    }
+
+    public void onUnitComplete(Unit unit) {
+        if (unit.getType().isWorker()) {
+            Unit closestMineral = null;
+            int closestDistance = Integer.MAX_VALUE;
+            for (Unit mineral : game.getMinerals()) {
+                int distance = unit.getDistance(mineral);
+                if (distance < closestDistance) {
+                    closestMineral = mineral;
+                    closestDistance = distance;
+                }
+            }
+            // Gather the closest mineral
+            unit.gather(closestMineral);
+        }
+    }
+}
